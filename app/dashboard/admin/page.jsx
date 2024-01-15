b606da22b88b3b761864346c70d0321510db07cb
Hercules <hsy.krmn@gmail.com>
2024-01-16 00:47:02 +0300

    edit layout

--- app/dashboard/admin/page.jsx
+++ app/dashboard/admin/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Page = () => {
-    return <div className="flex h-screen w-fit">
-      asdasd
-    </div>;
+    return (
+        <div className="flex h-screen w-full">
+            <p className="text-3xl border-b-2 mb-5 w-full h-12 text-center md:text-start">Admin DashBoard</p>
+        </div>
+    );
 };
 
 export default Page;
