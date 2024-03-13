f0121ec0e77d34c576bed6de477193b855fb1a79
Hercules <hsy.krmn@gmail.com>
2024-03-13 18:34:29 +0300

    update middleware

--- middleware.js
+++ middleware.js
@@ -7,18 +7,9 @@ export const config = {
 };
 
 export default withAuth(
-    async function middleware(req,res) {
+    async function middleware(req) {
         const url = req.nextUrl.pathname;
         const userRole = req?.nextauth?.token?.user?.role;
-
-        // add the CORS headers to the response
-        res.headers.append("Access-Control-Allow-Credentials", "true");
-        res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
-        res.headers.append("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
-        res.headers.append(
-            "Access-Control-Allow-Headers",
-            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-        );
 
         if (url.includes("/admin") && userRole !== "admin") {
             return NextResponse.redirect(new URL("/", req.url));
